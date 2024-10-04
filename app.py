c813b3a534d379fd6b2b83db8121b84b54957883
Layyyth <laithsocials@gmail.com>
2024-10-04 22:28:36 +0300

    Fix float to string conversion in ingredients column

--- app.py
+++ app.py
@@ -28,9 +28,10 @@ allergen_mapping = load_allergen_mapping(allergen_csv_path)
 # Load the meals data from 'Meals.csv'
 def load_meals(meals_csv_path):
     meals_df = pd.read_csv(meals_csv_path, encoding='ISO-8859-1')  # Specify encoding to avoid UnicodeDecodeError
-    # Replace any NaN or invalid entries with an empty string
-    meals_df['ingredients'] = meals_df['ingredients'].fillna('').apply(lambda x: [i.strip().lower() for i in str(x).split(',')])
+    # Add a check to handle non-string values
+    meals_df['ingredients'] = meals_df['ingredients'].apply(lambda x: [i.strip().lower() for i in str(x).split(',')] if isinstance(x, str) else [])
     return meals_df
+
 
 
 meals_csv_path = 'finalMeals.csv'
