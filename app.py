a8d0ad741cc1cfbb07626de683d4bdd3bcd5eb77
Layyyth <laithsocials@gmail.com>
2024-10-04 22:41:48 +0300

    Fix

--- app.py
+++ app.py
@@ -26,10 +26,13 @@ allergen_csv_path = 'finalAllergens.csv'
 allergen_mapping = load_allergen_mapping(allergen_csv_path)
 
 # Load the meals data from 'Meals.csv'
+# Load the meals data from 'Meals.csv'
 def load_meals(meals_csv_path):
     meals_df = pd.read_csv(meals_csv_path, encoding='ISO-8859-1')  
+    # Add a check to handle non-string values and NaNs
     meals_df['ingredients'] = meals_df['ingredients'].apply(lambda x: [i.strip().lower() for i in str(x).split(',')] if isinstance(x, str) else [])
     return meals_df
+
 
 meals_csv_path = 'finalMeals.csv'
 meals_df = load_meals(meals_csv_path)
